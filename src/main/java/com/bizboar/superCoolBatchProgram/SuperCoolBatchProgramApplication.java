06ef69fa63b7915c67d971e3370094cbaea51c7e
Travis J <tfauvelle@gmail.com>
2023-01-20 10:33:32 -0500

    init with basic auth

---
+++ src/main/java/com/bizboar/superCoolBatchProgram/SuperCoolBatchProgramApplication.java
@@ -0,0 +1,13 @@
+package com.bizboar.superCoolBatchProgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCoolBatchProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperCoolBatchProgramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bizboar/superCoolBatchProgram/SuperCoolBatchProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bizboar.superCoolBatchProgram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCoolBatchProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
